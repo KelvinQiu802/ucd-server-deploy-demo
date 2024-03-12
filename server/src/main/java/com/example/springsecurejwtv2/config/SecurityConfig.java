df45c9efc6615666afae8e473f78c7241372cf15
KelvinQiu <kelvinqiu802@outlook.com>
2024-03-12 12:16:37 +0800

    feat: enable CORS

--- server/src/main/java/com/example/springsecurejwtv2/config/SecurityConfig.java
+++ server/src/main/java/com/example/springsecurejwtv2/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -33,6 +34,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(
                         req -> req.requestMatchers("/api/auth/**").permitAll()
                                 .anyRequest().authenticated()
